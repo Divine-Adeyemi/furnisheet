63ea11db9cb23c7e260ee2c1870f4c792474501d
Divine Adeyemi <111776409+Divine-Adeyemi@users.noreply.github.com>
2025-02-07 19:52:07 +0100

    added a chat box and hamburger menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+    
     function myFunction() {
-        const menu = document.querySelector(".menu-left");
-        console.log("Menu toggled"); 
-        menu.classList.toggle("show"); // Toggle the "show" class
+        var x = document.querySelector(".menu-left"); // Get first element
+        
+        if (x) {
+            x.classList.toggle("show"); }
+
+        }
+        function openLocationModal() {
+    document.getElementById("locationModal").style.display = "block";
+}
+
+function closeLocationModal() {
+    document.getElementById("locationModal").style.display = "none";
+}
+
+// Close modal when clicking outside the container
+window.onclick = function(event) {
+    var modal = document.getElementById("locationModal");
+    if (event.target !== modal && !modal.contains(event.target) && event.target.tagName !== "A") {
+        modal.style.display = "none";
     }
+};
+document.getElementById("needHelpBtn").addEventListener("click", function () {
+  document.getElementById("chatbox").style.display = "block";
 });
+
+document.getElementById("closeChatbox").addEventListener("click", function () {
+  document.getElementById("chatbox").style.display = "none";
+});
+
+    
